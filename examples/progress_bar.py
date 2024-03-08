788239f632982a9991a27be1b7811f6ffccb6c64
CasuallyCalm <void@some.where>
2024-03-08 14:46:04 -0800

    more comments

--- examples/progress_bar.py
+++ examples/progress_bar.py
@@ -1,7 +1,7 @@
-import asyncio
+# reference: https://github.com/my1e5/dpg-examples/blob/main/threading/progress_bar.py
 
-# import time
-from datetime import datetime
+import asyncio
+import time
 
 import dearpygui.dearpygui as dpg
 from dearpygui_async import DearPyGuiAsync
@@ -14,8 +14,12 @@ running = False
 paused = False
 progress = 0
 
-async def run_task():
-    start = datetime.now()
+# This task is blocking due to time.sleep. Using asyncio.sleep would allow you to do this
+# without blocking the event loop, but adds additional time overhead due to the nature of asyncio
+# To convert this to not block the event loop are commented
+def run_task():
+# async def run_task
+    start = time.time() 
     global running
     global paused
     global progress
@@ -23,22 +27,23 @@ async def run_task():
     
     for i in range(1,101):
         while paused:
-            await asyncio.sleep(0.1)
+            # await asyncio.sleep(0.1)
+            time.sleep(0.1)
         if not running:
             return
         progress = i
         print(i)
         dpg.set_value(progress_bar, 1/100 * (i))
         dpg.configure_item(progress_bar, overlay=f"{i}%")
-        await asyncio.sleep(0.05)
+        # await asyncio.sleep(0.5)
+        time.sleep(0.05)
 
     print("Finished")
     running = False
     dpg.set_item_label(start_pause_resume_button, "Finished")
     dpg.disable_item(start_pause_resume_button)
     dpg.show_item(reset_button)
-    end = datetime.now()
-    print(f'Total time: {end-start}')
+    print(f'Total time: {time.time()-start}')
 
 def start_stop_callback():
     global running
@@ -47,7 +52,13 @@ def start_stop_callback():
         print("Started")
         running = True
         paused = False
-        asyncio.create_task(run_task())
+        # these two lines perform essentially the same way to run blocking
+        # code without blocking the main event loop
+        #--------------------------------
+        # dpg_async.loop.run_in_executor(None, run_task)
+        asyncio.create_task(asyncio.to_thread(run_task))
+        #-----------------------------
+        # asyncio.create_task(run_task()) # uncomment below to run an the non-blocking version and comment out the previous lines to run the blocking version
         dpg.set_item_label(start_pause_resume_button, "Pause")
     else:
         if not paused:
@@ -85,6 +96,5 @@ dpg.set_primary_window(primary_window, True)
 dpg.create_viewport(width=400, height=300, title="Progress Bar with Pause/Resume")
 dpg.setup_dearpygui()
 dpg.show_viewport()
-# dpg.start_dearpygui()
 dpg_async.run()
 dpg.destroy_context()
